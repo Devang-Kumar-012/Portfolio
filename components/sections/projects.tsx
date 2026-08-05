e4f19405aab84c1e663a714caa2e8b8c069ea97e
Devang Kumar <devangkumar012@gmail.com>
2026-08-05 21:41:40 +0530

    updated projects.tsx

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -160,6 +160,7 @@ export default function Projects() {
                             ))}
                         </div>
                     </motion.div>
+
                 )}
             </div>
 
